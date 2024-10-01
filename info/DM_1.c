cb83683e2efebf9c72cee0763514dd335a58ad1a
ZzKokonutzZ <159038783+ZzKokonutzZ@users.noreply.github.com>
2024-10-01 22:38:05 +0200

    DM 1

--- info/DM_1.c
+++ info/DM_1.c
@@ -89,7 +89,99 @@ int extraire(char string[])
 }
 
 //exo 15
+double sommemajo(double tab[],int n)
+{
+    int nb_p=0;
+    int nb_n=0;
+    double result_p=0;
+    double result_n=0;
+    for(int i=0;i<n;i+=1)
+    {
+        if (tab[i]>0)
+        {
+            result_p+=tab[i];
+            nb_p+=1;
+        }
+        if (tab[i]<0)
+        {
+            result_n+=tab[i];
+            nb_n+=1;
+        }
+    }
+    if (nb_p>nb_n)
+    {
+        return(result_p);
+    }
+    if (nb_n>nb_p)
+    {
+        return(result_n);
+    }
+    return(0);
+}
 
+//exo 16 (on supposera que seuls les 95 caractères usuels de la table ASCII sont présents dans la chaîne)
+bool charinstring(char c,char string[])
+{
+    int i=0;
+    bool in=false;
+    int l = len(string);
+    while (i<l && !in)
+    {
+        if (string[i]==c) in=true;
+        i+=1;
+    }
+    return(in);
+}
+int nbdiff(char string[])
+{
+    int nb=0;
+    char seen_chars[96];
+    seen_chars[0]='\0';
+    int i=0;
+    int l=len(string);
+    while(i<l && nb<95)
+    {
+        if(!charinstring(string[i],seen_chars))
+        {
+            seen_chars[nb]=string[i];
+            nb+=1;
+            seen_chars[nb]='\0';
+        }
+        i+=1;
+    }
+    return(nb);
+}
+
+//exo 17
+int power(int a,int b)
+{
+    if (b==0) return(1);
+    if (b==1) return(a);
+    if (b%2==0) return(power(a*a,b/2));
+    return(a*power(a*a,(b-1)/2));
+}
+bool palindromeint(int nb)
+{
+    int power_10=0;
+    int holder=nb;
+    while (holder>=10)
+    {
+        holder/=10;
+        power_10+=1;
+    }
+    int i=0;
+    while(i<=power_10/2)
+    {
+        int i_digit=(nb/power(10,i))-10*(nb/power(10,i+1));
+        int p_digit=(nb/power(10,power_10-i))-10*(nb/power(10,power_10-i+1));
+        if (i_digit!=p_digit)
+        {
+            return(false);
+        }
+        i+=1;
+    }
+    return(true);
+}
 
 int main()
 {
@@ -119,6 +211,24 @@ int main()
 
     printf("\n[exo 14] \n");
     printf("le premier entier dans la chaine \"lorem ipsum 42\" est : %d",extraire("lorem ipsum 42"));
+    printf("le premier entier dans la chaine \"lorem ipsum -42 42\" est : %d",extraire("lorem ipsum -42 42"));
+
+    printf("\n[exo 15] \n");
+    double tab2[]={-1.0,2.0,5.0,1.0,-25.0};
+    printf("la somme des elements majoritaires de tab2 est : %f \n",sommemajo(tab2,5));
+    double tab3[]={-1.0,2.0,5.0,-1.0,-25.0};
+    printf("la somme des elements majoritaires de tab3 est : %f \n",sommemajo(tab3,5));
+    double tab4[]={2.0,5.0,-1.0,-25.0};
+    printf("la somme des elements majoritaires de tab3 est : %f \n",sommemajo(tab4,4));
+
+    printf("\n[exo 16] \n");
+    char string[]="Hello World !";
+    printf("le nombre de caracteres differents dans %s est : %d",string,nbdiff(string));
+
+    printf("\n[exo 17] \n");
+    printf("42188124 est un palindrome : %d \n",palindromeint(42188124));
+    printf("4218124 est un palindrome : %d \n",palindromeint(4218124));
+    printf("4218 est un palindrome : %d \n",palindromeint(4218));
 
     return(0);
 }
